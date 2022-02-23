eef00f126dfba4efc06bdd686efbf86586eceaaf
Eskinott <heisesd@gmail.com>
2022-02-23 22:32:51 +0800

    i dont know what i commited

--- Assets/Script/playerControl/playerController.cs
+++ Assets/Script/playerControl/playerController.cs
@@ -7,9 +7,12 @@ public class playerController : MonoBehaviour
     // Start is called before the first frame update
     public GameObject player;
     public float movementSpeed = 1.0f;
+    public GameObject BackpackPanel;
     void Start()
     {
-        
+        player = GetComponent<GameObject>();
+        Transform canvas = GameObject.Find("Canvas").GetComponent<Transform>();
+        BackpackPanel = canvas.GetChild(0).gameObject;
     }
 
     // Update is called once per frame
@@ -18,5 +21,8 @@ public class playerController : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         transform.position = transform.position + new Vector3(horizontalInput * movementSpeed * Time.deltaTime, 0, verticalInput * movementSpeed * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.B)){
+            BackpackPanel.SetActive(!BackpackPanel.activeSelf);
+        }
     }
 }
