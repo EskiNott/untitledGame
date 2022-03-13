4c34377edd963e718ec6a9452ff4b2b25240c018
EskiNott <heisesd@gmail.com>
2022-03-13 09:44:43 +0800

    fixing rotation bug

--- Assets/Script/Event/InteractWithObject/investigate.cs
+++ Assets/Script/Event/InteractWithObject/investigate.cs
@@ -8,6 +8,11 @@ public class investigate : MonoBehaviour,IPointerClickHandler
     private Vector3 tempPosition;
     private Quaternion tempRotation;
     private bool thisInvestigate;
+    private Vector2 MousePos1;
+    private Vector2 MousePos2;
+    private Vector3 tempRotationDragging;
+    private bool isDragging = false;
+
     public bool test = false;
     public Camera cam;
     public float distance = 1.0f;
@@ -24,24 +29,33 @@ public class investigate : MonoBehaviour,IPointerClickHandler
     // Update is called once per frame
     void Update()
     {
-        if (globalManager.GetComponent<GlobalManager>().isInvestigate && Input.GetMouseButton(0) && thisInvestigate)
+        if (globalManager.GetComponent<GlobalManager>().isInvestigate && thisInvestigate && (Input.GetMouseButton(0) || Input.GetMouseButtonDown(0) || Input.GetMouseButtonUp(0)))
         {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-            test = true;
-            if (Physics.Raycast(ray, out hit))
+            if (Input.GetMouseButtonDown(0) && Physics.Raycast(ray, out hit) && (hit.collider.gameObject == gameObject)) 
             {
-                if (hit.collider.gameObject != gameObject) 
+                tempRotationDragging = transform.rotation.eulerAngles;
+                MousePos1 = Input.mousePosition;
+                isDragging = true;
+            }
+            else if (Input.GetMouseButtonDown(0) && Physics.Raycast(ray, out hit) && !isDragging)
+            {
+                if (hit.collider.gameObject != gameObject)
                 {
                     transform.position = tempPosition;
                     transform.rotation = tempRotation;
                     thisInvestigate = false;
                     globalManager.GetComponent<GlobalManager>().isInvestigate = false;
                 }
-                else
-                {
-
-                }
+            }
+            else if (Input.GetMouseButton(0))
+            {
+                MousePos2 = Input.mousePosition;
+                transform.eulerAngles = new Vector3(tempRotationDragging.x + MousePos1.y - MousePos2.y, tempRotationDragging.y + +MousePos1.x - MousePos2.x, 0);
+            }else if (Input.GetMouseButtonUp(0))
+            {
+                isDragging = false;
             }
         }
     }
